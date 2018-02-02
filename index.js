3d38d0434b758940f1e59e9e76ee2b0707065c16
Dmitry Kunin <dkunin@avito.ru>
2018-02-02 09:14:29 +0300

    Update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express    = require('express');
 var Webtask    = require('webtask-tools');
 var bodyParser = require('body-parser');
@@ -5,8 +7,17 @@ var app = express();
 
 app.use(bodyParser.json());
 
+function generate(flags) {
+    const wc = flags.wc || 4;
+    const s = flags.s || '_';
+    const words = new Array(wc)
+        .fill()
+        .map(() => uniqueRandomArray(mnemonicWords)());
+    return words.join(s);
+}
+
 app.get('/', function (req, res) {
-  res.sendStatus(200);
+  res.send(generate({}));
 });
 
 module.exports = Webtask.fromExpress(app);
